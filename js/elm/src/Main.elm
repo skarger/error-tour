e81af635917220b1cc0bf7b087cf72c483a2d57c
Stephen Karger <stephen.karger@gmail.com>
2020-02-07 17:52:10 -0500

    Elm example working

--- js/elm/src/Main.elm
+++ js/elm/src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, pre, text)
-import Http
+import Html exposing (Html, button, div, pre, span, text, textarea)
+import Html.Attributes exposing (cols, placeholder, rows)
+import Html.Events exposing (onClick, onInput)
+import Json.Decode exposing (Decoder, decodeString, field, map, string)
 
 
 main =
@@ -14,51 +16,103 @@ main =
         }
 
 
-type Model
-    = Failure
-    | Loading
-    | Success String
+type alias Model =
+    { input : String
+    , status : Status
+    , cat : Maybe Cat
+    }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Loading
-    , Http.get
-        { url = "https://elm-lang.org/assets/public-opinion.txt"
-        , expect = Http.expectString GotText
-        }
-    )
+type Status
+    = Waiting
+    | Failure
+    | Success
+
+
+type alias Cat =
+    { name : String
+    }
 
 
 type Msg
-    = GotText (Result Http.Error String)
+    = EnterJSON String
+    | ParseJSON
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotText result ->
-            case result of
-                Ok fullText ->
-                    ( Success fullText, Cmd.none )
+        EnterJSON str ->
+            ( { model | cat = model.cat, status = Waiting, input = str }, Cmd.none )
+
+        ParseJSON ->
+            let
+                parseResult =
+                    decodeString catDecoder model.input
+            in
+            case parseResult of
+                Ok cat ->
+                    ( { model | cat = Just cat, status = Success }, Cmd.none )
 
                 Err _ ->
-                    ( Failure, Cmd.none )
+                    ( { model | status = Failure }, Cmd.none )
 
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+catDecoder : Decoder Cat
+catDecoder =
+    map Cat
+        (field "name" string)
 
 
 view : Model -> Html Msg
 view model =
-    case model of
+    div []
+        [ div [] <| inputView model
+        , div [] <| resultView model
+        ]
+
+
+inputView : Model -> List (Html Msg)
+inputView model =
+    [ textarea [ rows 20, cols 40, placeholder "Enter JSON...", onInput EnterJSON ] []
+    , div []
+        [ span [] [ text "Input:" ]
+        , pre [] [ text model.input ]
+        ]
+    , button [ onClick ParseJSON ] [ text "Parse JSON" ]
+    ]
+
+
+resultView : Model -> List (Html Msg)
+resultView model =
+    case model.status of
         Failure ->
-            text "I was unable to load your book."
+            [ text "Could not parse cat from given input." ]
 
-        Loading ->
-            text "Loading..."
+        Success ->
+            [ text <| "Parse succeeded. Cat name: " ++ getCatName model.cat ]
 
-        Success fullText ->
-            pre [] [ text fullText ]
+        Waiting ->
+            [ text "" ]
+
+
+getCatName : Maybe Cat -> String
+getCatName mcat =
+    case mcat of
+        Just cat ->
+            cat.name
+
+        Nothing ->
+            ""
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { input = "", status = Waiting, cat = Nothing }
+    , Cmd.none
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
