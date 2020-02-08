4476271549799b6909b2a76c1e4f7120a504e7fc
Stephen Karger <stephen.karger@gmail.com>
2020-02-08 11:33:48 -0500

    rails new --skip-git --skip-action-cable --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-active-storage --skip-spring --skip-javascript --skip-sprockets --skip-turbolinks --skip-test --api -d postgresql error-tour

--- rb/RubyExample.rb
+++
@@ -1,9 +0,0 @@
-begin
-  puts "Parsed #{Integer('1000')}."
-
-  puts "Parsed #{Integer('A grand')}." # 'A grand'.to_i would return nil
-rescue ArgumentError => e
-  puts "Error: #{e}."
-ensure
-  puts "Have a nice day."
-end
