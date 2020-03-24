7d131d5485c16ff7a1e4ac6c8dea17ff1143592d
Stephen Karger <stephen.karger@gmail.com>
2020-03-24 10:53:21 -0400

    Drop result-type

--- js/app/router.js
+++ js/app/router.js
@@ -7,7 +7,6 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('result-type');
   this.route('sound-principles');
   this.route('prevention');
   this.route('route-level');
