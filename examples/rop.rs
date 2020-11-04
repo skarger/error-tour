25feaeb1d48542349ad9d4c390fd69ba1efa2af0
Stephen Karger <stephen.karger@gmail.com>
2020-11-03 21:39:20 -0500

    Update examples

--- examples/rop.rs
+++ examples/rop.rs
@@ -7,11 +7,11 @@ fn main() {
 
 fn two_track_computation(input : &str) -> String {
     parse_number(input)
-        .map_err(|e| format!("Could not parse \"{}\": {}", input, e))
+        .map_err(|e| format!("{}: \"{}\"", e, input))
         .and_then(ensure_positive)
         .map(double)
-        .map_or_else(|error| format!("Failure: {}.", error),
-                     |number| format!("Success: computed {}.", number))
+        .map_or_else(|error| format!("Error: {}.", error),
+                     |number| format!("Success: computed {} from \"{}\".", number, input))
 }
 
 fn parse_number(value : &str) -> Result<u32, std::num::ParseIntError> {
@@ -22,7 +22,7 @@ fn ensure_positive(number : u32) -> Result<u32, String> {
     if number > 0 {
         Ok(number)
     } else {
-        Err(format!("{} is not positive", number))
+        Err(format!("\"{}\" is not positive", number))
     }
 }
 
